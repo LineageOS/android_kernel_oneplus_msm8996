99ac2d630b41f9eba5892250119997c4ecfcc574
Oleg Perelet <operelet@codeaurora.org>
2016-02-11 14:07:52 -0800

    msm: kgsl: Disable all HW clockgating during snapshot
    While producing snapshot disable all HWCG branches, not only top level.
    
    CRs-Fixed: 978122
    Change-Id: I4b01224a0ba46c276115a284a0da6207c7968f72
    Signed-off-by: Oleg Perelet <operelet@codeaurora.org>

--- drivers/gpu/msm/adreno_a5xx_snapshot.c
+++ drivers/gpu/msm/adreno_a5xx_snapshot.c
@@ -809,9 +809,6 @@ void a5xx_snapshot(struct adreno_device *adreno_dev,
 	/* Try to run the crash dumper */
 	_a5xx_do_crashdump(device);
 
-	/* Try to run the crash dumper */
-	_a5xx_do_crashdump(device);
-
 	kgsl_snapshot_add_section(device, KGSL_SNAPSHOT_SECTION_REGS,
 		snapshot, a5xx_snapshot_registers, NULL);
 
