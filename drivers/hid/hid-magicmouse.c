cff9116b4afb0f21222fe864248225bd0d8621c1
dianlujitao <dianlujitao@gmail.com>
2016-06-26 14:47:32 +0800

    HID: Fix compilation
    Change-Id: I8b9fe983652989c3f6679ed08743f1d5b12b477a

--- drivers/hid/hid-magicmouse.c
+++ drivers/hid/hid-magicmouse.c
@@ -471,18 +471,20 @@ static int magicmouse_input_mapping(struct hid_device *hdev,
 	return 0;
 }
 
-static void magicmouse_input_configured(struct hid_device *hdev,
+static int magicmouse_input_configured(struct hid_device *hdev,
 		struct hid_input *hi)
 
 {
 	struct magicmouse_sc *msc = hid_get_drvdata(hdev);
+	int ret = 0;
 
-	int ret = magicmouse_setup_input(msc->input, hdev);
+	ret = magicmouse_setup_input(msc->input, hdev);
 	if (ret) {
 		hid_err(hdev, "magicmouse setup input failed (%d)\n", ret);
 		/* clean msc->input to notify probe() of the failure */
 		msc->input = NULL;
 	}
+	return ret;
 }
 
 
