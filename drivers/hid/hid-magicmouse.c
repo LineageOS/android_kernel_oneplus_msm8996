baf86cc64e3c8141dcbf4efd3e7f46a471d6cccd
Steve Kondik <steve@cyngn.com>
2016-07-21 00:00:03 -0700

    input: magicmouse: Fix compilation issue
    Change-Id: Ica31d61e1c5eb2707a6ac3d8d88ab719fccb90fd

--- drivers/hid/hid-magicmouse.c
+++ drivers/hid/hid-magicmouse.c
@@ -471,7 +471,7 @@ static int magicmouse_input_mapping(struct hid_device *hdev,
 	return 0;
 }
 
-static int magicmouse_input_configured(struct hid_device *hdev,
+static void magicmouse_input_configured(struct hid_device *hdev,
 		struct hid_input *hi)
 
 {
@@ -484,7 +484,6 @@ static int magicmouse_input_configured(struct hid_device *hdev,
 		/* clean msc->input to notify probe() of the failure */
 		msc->input = NULL;
 	}
-	return ret;
 }
 
 
