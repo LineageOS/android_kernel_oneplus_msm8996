70a71453848debf98dbf28b71bf95dc7aa41a5ba
Steve Kondik <steve@cyngn.com>
2016-09-08 13:04:45 -0700

    msm:camera: Enable/Disable camera daemon
    Camera daemon process is moving to mediaserver
    process. MCT will send contol command
    to indicate camera daemon is present or not.
    
    For backward compatibility, add check
    so kernel can support camera daemon present
    and camera daemon not present.
    
    Change-Id: Ia9e45f539bd5ec0d2edfe9bfb90942d88b6d30da
    Signed-off-by: Jignesh Mehta <jigneshm@codeaurora.org>

--- include/media/msmb_camera.h
+++ include/media/msmb_camera.h
@@ -23,6 +23,9 @@
 #define MSM_CAM_V4L2_IOCTL_NOTIFY_DEBUG \
 	_IOW('V', BASE_VIDIOC_PRIVATE + 34, struct msm_v4l2_event_data)
 
+#define MSM_CAM_V4L2_IOCTL_DAEMON_DISABLED \
+	_IOW('V', BASE_VIDIOC_PRIVATE + 35, struct msm_v4l2_event_data)
+
 #ifdef CONFIG_COMPAT
 #define MSM_CAM_V4L2_IOCTL_NOTIFY32 \
 	_IOW('V', BASE_VIDIOC_PRIVATE + 30, struct v4l2_event32)
