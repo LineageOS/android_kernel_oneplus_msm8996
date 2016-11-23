937d5e169ce464de8bec677e0c099a96951078f3
Alexander Martinz <eviscerationls@gmail.com>
2016-11-23 21:17:08 +0100

    Revert "sdcardfs: Added top to sdcardfs_inode_info"
    This reverts commit 4eb1278c69b6b14d4f2e3ede0d5a9e493a1a2ca4.
    
    Change-Id: I46b7c3a6aa7145f02158a33b7130df3ab9de5dff
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- fs/sdcardfs/packagelist.c
+++ fs/sdcardfs/packagelist.c
@@ -157,9 +157,9 @@ static int insert_str_to_int_lock(struct packagelist_data *pkgl_dat, char *key,
 	return 0;
 }
 
-static void fixup_perms(struct super_block *sb, const char *key) {
+static void fixup_perms(struct super_block *sb) {
 	if (sb && sb->s_magic == SDCARDFS_SUPER_MAGIC) {
-		fixup_perms_recursive(sb->s_root, key, strlen(key));
+		get_derive_permissions_recursive(sb->s_root);
 	}
 }
 
@@ -174,7 +174,7 @@ static int insert_str_to_int(struct packagelist_data *pkgl_dat, char *key,
 
 	list_for_each_entry(sbinfo, &sdcardfs_super_list, list) {
 		if (sbinfo) {
-			fixup_perms(sbinfo->sb, key);
+			fixup_perms(sbinfo->sb);
 		}
 	}
 	mutex_unlock(&sdcardfs_super_list_lock);
@@ -203,7 +203,7 @@ static void remove_str_to_int(struct packagelist_data *pkgl_dat, const char *key
 	spin_unlock(&pkgl_data_all->hashtable_lock);
 	list_for_each_entry(sbinfo, &sdcardfs_super_list, list) {
 		if (sbinfo) {
-			fixup_perms(sbinfo->sb, key);
+			fixup_perms(sbinfo->sb);
 		}
 	}
 	mutex_unlock(&sdcardfs_super_list_lock);
