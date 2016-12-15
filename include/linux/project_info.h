917e34728992385078dbbe5893a8754027dd5284
Willi Ye <williye97@gmail.com>
2016-12-15 15:33:42 +0100

    Import 15811
    * Bring cpr3 up to date
    
    amartinz:
    * update synaptics driver
    
    Change-Id: I7d42029d31109b350eaa448a604c78b7af0f571e

--- include/linux/project_info.h
+++ include/linux/project_info.h
@@ -1,5 +1,26 @@
 #ifndef _PROJECT_INFO_H_
 #define _PROJECT_INFO_H_ 1
+typedef		__u32		uint32;
+
+struct project_info{
+       char project_name[8];  //eg, 14049
+       uint32  hw_version;  //PCB number, T0, EVT
+       uint32  rf_v1;   //v1 for mainboard_rf_version
+       uint32  rf_v2;   //v2 for aboard_rf_version
+       uint32  rf_v3;
+       uint32  modem;
+       uint32  operator;
+       uint32  ddr_manufacture_info;
+       uint32  ddr_raw;
+       uint32  ddr_column;
+       uint32  ddr_reserve_info;
+	   uint32  platform_id;
+};
+
+struct component_info{
+	char *version;
+	char *manufacture;
+};
 
 enum COMPONENT_TYPE{
 	DDR,
@@ -22,10 +43,12 @@ enum COMPONENT_TYPE{
 	ABOARD,
 	NFC,
 	FAST_CHARGE,
+	CPU,
 	COMPONENT_MAX,
 };
 
 int push_component_info(enum COMPONENT_TYPE type, char *version, char * manufacture);
 int reset_component_info(enum COMPONENT_TYPE type);
+uint32 get_hw_version(void);
 
 #endif
