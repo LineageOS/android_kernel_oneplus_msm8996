b01fc2615412f8b9c70df6072f4f19ab2ccbade2
Tim Murray <timmurray@google.com>
2017-01-18 13:57:46 -0800

    op3: enable additional RCU debugging
    bug 34193533
    Tests: boots
    
    Change-Id: Idf207e62a79d73c02ccc9f67804139a8e783a288

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -1124,6 +1124,8 @@ static void print_other_cpu_stall(struct rcu_state *rsp, unsigned long gpnum)
 
 	/* Complain about tasks blocking the grace period. */
 
+	trigger_allbutself_cpu_backtrace();
+
 	rcu_print_detail_task_stall(rsp);
 
 	panic_on_rcu_stall();
