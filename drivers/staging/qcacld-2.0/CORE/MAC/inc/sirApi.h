3e0a2075526e20b92787dca60b1c8eec2e1ed2b1
dianlujitao <dianlujitao@lineageos.org>
2017-03-11 11:54:49 +0800

    qcacld-2.0: Update to LA.UM.5.5.r1-03600-8x96.0
    Change-Id: Ie5b173d85700672942848de53f4c0430840880d1

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/sirApi.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/sirApi.h
@@ -1722,6 +1722,15 @@ typedef struct sSirSmeDeauthCnf
     tSirMacAddr        peerMacAddr;
 } tSirSmeDeauthCnf, *tpSirSmeDeauthCnf;
 
+/* Definition for disconnect done indication */
+typedef struct sSirSmeDisConDoneInd {
+   tANI_U16           messageType;
+   tANI_U16           length;
+   tANI_U8            sessionId;
+   tSirResultCodes    reasonCode;
+   tSirMacAddr        peerMacAddr;
+} tSirSmeDisConDoneInd, *tpSirSmeDisConDoneInd;
+
 /// Definition for stop BSS request message
 typedef struct sSirSmeStopBssReq
 {
@@ -4468,6 +4477,7 @@ typedef struct sSirScanOffloadReq {
     tSirP2pScanType p2pScanType;
     tANI_U16 uIEFieldLen;
     tANI_U16 uIEFieldOffset;
+    uint32_t burst_scan_duration;
     tSirChannelList channelList;
     /*-----------------------------
       sSirScanOffloadReq....
