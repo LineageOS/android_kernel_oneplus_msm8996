48266063bb1e9ac3e55d4dccfd1572ea8d63ff47
dianlujitao <dianlujitao@lineageos.org>
2018-02-10 22:27:44 +0800

    msm: camera: sensor: Fix video node creation
     * The bypass_video_node_creation variable is assigned too early,
       in some cases the invalid slave_info is used by msm_sensor_driver_probe
       before the valid ones, and video node creation for the valid ones
       will be skipped.
     * Assign bypass_video_node_creation after probe succeed to fix it.
    
    Change-Id: Ieb1115c6a8c89820f6c1a220d741d955d88bc26c

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -972,9 +972,6 @@ CSID_TG:
 
 	pr_err("%s probe succeeded", slave_info->sensor_name);
 
-	s_ctrl->bypass_video_node_creation =
-		slave_info->bypass_video_node_creation;
-
 	/*
 	 * Update the subdevice id of flash-src based on availability in kernel.
 	 */
@@ -1032,6 +1029,10 @@ CSID_TG:
 	 * probed on this slot
 	 */
 	s_ctrl->is_probe_succeed = 1;
+
+	s_ctrl->bypass_video_node_creation =
+		slave_info->bypass_video_node_creation;
+
 	return rc;
 
 camera_power_down:
