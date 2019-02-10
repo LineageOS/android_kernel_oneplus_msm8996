298deed63255b2ad4e095d7e6b40b8786bcc8cfd
dianlujitao <dianlujitao@lineageos.org>
2019-02-10 20:02:17 +0800

    msm: camera: Fix compilation
    Change-Id: I55a814e5e0f254ebe40fbe9e12600f0d5786bc22

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -2249,8 +2249,10 @@ static void msm_isp_get_camif_update_state_and_halt(
 		axi_data->src_info[VFE_PIX_0].pix_stream_count +
 		axi_data->src_info[VFE_PIX_0].raw_stream_count;
 
-	if (stream_cfg_cmd->num_streams > VFE_AXI_SRC_MAX)
+	if (stream_cfg_cmd->num_streams > VFE_AXI_SRC_MAX) {
+		*camif_update = NO_UPDATE;
 		return;
+	}
 
 	for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 		stream_info =
